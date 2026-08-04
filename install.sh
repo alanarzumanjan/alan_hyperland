c7d2ce68b11e6ae2b54a91acee144a50db07a846
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-08-04 22:01:58 +0300

    Add install script for configuration symlinks

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# install.sh
+ln -sf /data/alan_hyperland/config/hypr ~/.config/hypr
+ln -sf /data/alan_hyperland/config/waybar ~/.config/waybar
+ln -sf /data/alan_hyperland/config/kitty ~/.config/kitty
+ln -sf /data/alan_hyperland/config/fish ~/.config/fish
+ln -sf /data/alan_hyperland/config/starship.toml ~/.config/starship.toml
+ln -sf /data/alan_hyperland/dotfiles/.bashrc ~/.bashrc
+ln -sf /data/alan_hyperland/dotfiles/.gitconfig ~/.gitconfig
+ln -sf /data/alan_hyperland/bin/random-wallpaper.sh ~/bin/random-wallpaper.sh
