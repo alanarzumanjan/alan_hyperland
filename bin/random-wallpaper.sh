11c5fb5987e0b7a8a7ea4995f9cbd667d9100c84
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-08-08 00:37:10 +0300

    Update random-wallpaper script to prioritize default wallpaper; remove hyprpicker autostart from hyprland configuration

--- bin/random-wallpaper.sh
+++ bin/random-wallpaper.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 WALLPAPER_DIR=~/Media/Pictures/Wallpapers
+DEFAULT_WALL="$WALLPAPER_DIR/default.png"
+
 if ! pgrep -x "awww-daemon" > /dev/null; then
     awww-daemon &
     sleep 1
 fi
 
-RANDOM_WALL=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.gif" \) | shuf -n 1)
+if [ -f "$DEFAULT_WALL" ]; then
+    RANDOM_WALL="$DEFAULT_WALL"
+else
+    RANDOM_WALL=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.gif" \) | shuf -n 1)
+fi
 
 if [ -n "$RANDOM_WALL" ]; then
     awww img "$RANDOM_WALL"
