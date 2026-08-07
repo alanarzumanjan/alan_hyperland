c7afb2e78ee7ccd2c8090611d2102d6f4e275cad
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-08-08 00:37:10 +0300

    Update random-wallpaper script to prioritize default wallpaper; remove hyprpicker autostart from hyprland configuration

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -46,7 +46,6 @@ hl.on("hyprland.start", function ()
    hl.exec_cmd("waybar")
    hl.exec_cmd("awww-daemon") 
    hl.exec_cmd("~/bin/random-wallpaper.sh &")
-   hl.exec_cmd("hyprpicker -a &")
 end)
 
 -------------------------------
