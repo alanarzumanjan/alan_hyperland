c838a8c0a7d6962463ec3b43bdc0ce9eba3300e8
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-08-08 23:57:50 +0300

    Add hypridle and hyprsunset to autostart applications

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -45,6 +45,8 @@ hl.on("hyprland.start", function ()
    hl.exec_cmd("nm-applet")
    hl.exec_cmd("waybar")
    hl.exec_cmd("awww-daemon") 
+   hl.exec_cmd("hypridle")
+   hl.exec_cmd("hyprsunset")
    hl.exec_cmd("~/bin/random-wallpaper.sh &")
 end)
 
