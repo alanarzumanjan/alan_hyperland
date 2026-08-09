d779c78825fa13b79241d53479475ee36c98d088
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-08-09 17:45:50 +0300

    Update hyprland and hyprpaper configurations: comment out logo disabling option and extend wallpaper timeout

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -205,7 +205,7 @@ hl.config({
 hl.config({
     misc = {
         force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-        disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
+        -- disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
     },
 })
 
