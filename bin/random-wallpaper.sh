ec83bf72d838dcbed5b76ca542b0461d4bff051a
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-08-11 16:50:02 +0300

    Update wallpaper directory and default wallpaper path for consistency

--- bin/random-wallpaper.sh
+++ bin/random-wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-WALLPAPER_DIR=~/Media/Pictures/Wallpapers
-DEFAULT_WALL="$WALLPAPER_DIR/default.png"
+WALLPAPER_DIR=/data/alan_home_ext/alan_hyperland/wallpapers
+DEFAULT_WALL="$WALLPAPER_DIR/1.png"
 
 if ! pgrep -x "awww-daemon" > /dev/null; then
     awww-daemon &
